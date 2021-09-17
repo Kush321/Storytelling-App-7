fb9a7e68b19050198f5445afbe7931c47e5b8c3f
Kush321 <78313670+Kush321@users.noreply.github.com>
2021-09-16 21:05:47 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBONAdc7kEtwXlFsbCCSrnGWPB_JUS8sFY",
+    authDomain: "storytelling-app-bce71.firebaseapp.com",
+    databaseURL: "https://storytelling-app-bce71-default-rtdb.firebaseio.com",
+    projectId: "storytelling-app-bce71",
+    storageBucket: "storytelling-app-bce71.appspot.com",
+    messagingSenderId: "904825255736",
+    appId: "1:904825255736:web:691320f40e2ce16f0f33cd"
+  };
+  
